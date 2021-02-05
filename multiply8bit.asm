669e48fe973f1d83902365b14b7e3d823ccd8bd0
sirius <himansh02@hotmail.com>
2021-02-05 19:24:47 +0530

    Update multiply8bit.asm

--- multiply8bit.asm
+++ multiply8bit.asm
@@ -9,23 +9,23 @@ org 100h                        ; sode starts from offset 100h
     
      
 .code                           ; code segment
-    MOV al, alpha
-    MOV bl, beta 
-    MUL bl   
-    ADD al, 48
+    MOV al, alpha               ; move value $alpha to @al
+    MOV bl, beta                ; move Value $beta to @bl
+    MUL bl                      ; multiply bl with al and store it in @al
+    ADD al, 48                  ; add 48 to al to represent the value @al in numerical format
     
-    MOV dl, al
-    MOV ah, 2h
-    INT 21h
+    MOV dl, al                  ; move the value @al into @dl
+    MOV ah, 2h                  ; function to print the value @dl
+    INT 21h                     ; DOS interrupt
+                                
+    MOV al, alpha               ; move value $alpha into @al
+    MOV cl, zeta                ; move value $zeta into @cl
+    MUL cl                      ; multiply cl with al and store it in @al
+    ADD al, 48                  ; add 48 to al to represent the value @al in numerical format
     
-    MOV al, alpha
-    MOV cl, zeta 
-    MUL cl   
-    ADD al, 48
-    
-    MOV dl, al
-    MOV ah, 2h
-    INT 21h
+    MOV dl, al                  ; move al into dl
+    MOV ah, 2h                  ; function to print the value @dl
+    INT 21h                     ; DOS interrupt
                             
 
 END                             ; End Program
